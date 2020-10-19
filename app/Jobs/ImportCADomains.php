86c2ad2860c7d6380195f5892d5527fcc36a62b7
Sarmad Sohail <sarmad-sohail@hotmail.com>
2020-10-20 09:04:13 +1200

    update

---
+++ app/Jobs/ImportCADomains.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Imports\DomainImport;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+use Maatwebsite\Excel\Facades\Excel;
+
+class ImportCADomains implements ShouldQueue
+{
+  use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+  private $date;
+
+  /**
+   * Create a new job instance.
+   *
+   * @return void
+   */
+  public function __construct($date)
+  {
+    $this->date = $date;
+  }
+
+  /**
+   * Execute the job.
+   *
+   * @return void
+   */
+  public function handle()
+  {
+    Excel::import(new DomainImport("Canada"), public_path(sprintf("whois/%s/country-specific-database/canada.csv", $this->date)));
+
+  }
+}
