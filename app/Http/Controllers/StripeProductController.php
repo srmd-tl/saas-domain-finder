1654a75cd7bd124fd93c3a68e7ed40e32b664cac
Sarmad Sohail <sarmad-sohail@hotmail.com>
2020-12-25 00:47:37 +0500

    product save function

--- app/Http/Controllers/StripeProductController.php
+++ app/Http/Controllers/StripeProductController.php
@@ -2,12 +2,39 @@
 
 namespace App\Http\Controllers;
 
+use App\StripeProduct;
+use App\Utils\StripeTraits;
 use Illuminate\Http\Request;
 
 class StripeProductController extends Controller
 {
-    public function store(Request $request)
-    {
-        $request->validate([""])
+  use StripeTraits;
+
+  public function store(Request $request)
+  {
+    $request->validate([
+      "name" => "required",
+      "price" => "required"
+    ]);
+    try {
+      $product = $this->storeProduct($request->name);
+    } catch (\Exception $e) {
+      return back()->withErrors($e->getMessage());
     }
+    try {
+      $price = $this->storePlan($request->price, $product->id);
+    } catch (\Exception $e) {
+      return back()->withErrors($e->getMessage());
+
+    }
+    StripeProduct::create([
+      "name" => $request->name,
+      "stripe_product_id" => $product->id,
+      "amount" => $request->amount,
+      "view" => $request->view,//how much domains he can view
+      "interval" => $request->interval,
+      "stripe_price_id" => $price->id
+    ]);
+    return back()->withSuccess("Product Saved");
+  }
 }
