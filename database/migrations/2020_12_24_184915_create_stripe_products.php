e6c2383ce4e4f55e1a4031af8f3b6a3cf733eef9
Sarmad Sohail <sarmad-sohail@hotmail.com>
2020-12-25 00:51:12 +0500

    migration updates

--- database/migrations/2020_12_24_184915_create_stripe_products.php
+++ database/migrations/2020_12_24_184915_create_stripe_products.php
@@ -6,26 +6,32 @@ use Illuminate\Support\Facades\Schema;
 
 class CreateStripeProducts extends Migration
 {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
-    public function up()
-    {
-        Schema::create('stripe_products', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
-        });
-    }
+  /**
+   * Run the migrations.
+   *
+   * @return void
+   */
+  public function up()
+  {
+    Schema::create('stripe_products', function (Blueprint $table) {
+      $table->id();
+      $table->timestamps();
+      $table->string("name");
+      $table->string("stripe_product_id");
+      $table->double("amount");
+      $table->integer("view")->comment("how much domains he can view");
+      $table->integer("interval");
+      $table->string("stripe_price_id");
+    });
+  }
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
-    public function down()
-    {
-        Schema::dropIfExists('stripe_products');
-    }
+  /**
+   * Reverse the migrations.
+   *
+   * @return void
+   */
+  public function down()
+  {
+    Schema::dropIfExists('stripe_products');
+  }
 }
