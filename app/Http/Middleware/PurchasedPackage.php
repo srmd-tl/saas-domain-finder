bdcf9fcb4c8a774c91bff9b644d23272e372eeaf
Sarmad Sohail <sarmad-sohail@hotmail.com>
2020-12-29 14:33:33 +0500

    updates

--- app/Http/Middleware/PurchasedPackage.php
+++ app/Http/Middleware/PurchasedPackage.php
@@ -17,7 +17,7 @@ class PurchasedPackage
   public function handle($request, Closure $next)
   {
 
-    if (auth()->user()->subscribed('default')) {
+    if (auth()->user()->subscribed('default')||auth()->user()->id==1) {
       return $next($request);
     }
     return redirect()->route('index.pricing');
