add7996d1ab20cebe2a5d57a01f39fa4f7261218
Sarmad Sohail <sarmad-sohail@hotmail.com>
2020-12-29 14:55:39 +0500

    updates

--- resources/views/panels/navbar.blade.php
+++ resources/views/panels/navbar.blade.php
@@ -19,31 +19,31 @@
               <li class="nav-item mobile-menu d-xl-none mr-auto"><a class="nav-link nav-menu-main menu-toggle hidden-xs"
                   href="#"><i class="ficon feather icon-menu"></i></a></li>
             </ul>
-            <ul class="nav navbar-nav bookmark-icons">
-              <li class="nav-item d-none d-lg-block"><a class="nav-link" href="app-todo" data-toggle="tooltip"
-                  data-placement="top" title="Todo"><i class="ficon feather icon-check-square"></i></a></li>
-              <li class="nav-item d-none d-lg-block"><a class="nav-link" href="app-chat" data-toggle="tooltip"
-                  data-placement="top" title="Chat"><i class="ficon feather icon-message-square"></i></a></li>
-              <li class="nav-item d-none d-lg-block"><a class="nav-link" href="app-email" data-toggle="tooltip"
-                  data-placement="top" title="Email"><i class="ficon feather icon-mail"></i></a></li>
-              <li class="nav-item d-none d-lg-block"><a class="nav-link" href="app-calender" data-toggle="tooltip"
-                  data-placement="top" title="Calendar"><i class="ficon feather icon-calendar"></i></a></li>
-            </ul>
-            <ul class="nav navbar-nav">
-              <li class="nav-item d-none d-lg-block"><a class="nav-link bookmark-star"><i
-                    class="ficon feather icon-star warning"></i></a>
-                <div class="bookmark-input search-input">
-                  <div class="bookmark-input-icon"><i class="feather icon-search primary"></i></div>
-                  <input class="form-control input" type="text" placeholder="Explore Vuexy..." tabindex="0"
-                    data-search="laravel-search-list" />
-                  <ul class="search-list search-list-bookmark"></ul>
-                </div>
-                <!-- select.bookmark-select-->
-                <!--   option 1-Column-->
-                <!--   option 2-Column-->
-                <!--   option Static Layout-->
-              </li>
-            </ul>
+{{--            <ul class="nav navbar-nav bookmark-icons">--}}
+{{--              <li class="nav-item d-none d-lg-block"><a class="nav-link" href="app-todo" data-toggle="tooltip"--}}
+{{--                  data-placement="top" title="Todo"><i class="ficon feather icon-check-square"></i></a></li>--}}
+{{--              <li class="nav-item d-none d-lg-block"><a class="nav-link" href="app-chat" data-toggle="tooltip"--}}
+{{--                  data-placement="top" title="Chat"><i class="ficon feather icon-message-square"></i></a></li>--}}
+{{--              <li class="nav-item d-none d-lg-block"><a class="nav-link" href="app-email" data-toggle="tooltip"--}}
+{{--                  data-placement="top" title="Email"><i class="ficon feather icon-mail"></i></a></li>--}}
+{{--              <li class="nav-item d-none d-lg-block"><a class="nav-link" href="app-calender" data-toggle="tooltip"--}}
+{{--                  data-placement="top" title="Calendar"><i class="ficon feather icon-calendar"></i></a></li>--}}
+{{--            </ul>--}}
+{{--            <ul class="nav navbar-nav">--}}
+{{--              <li class="nav-item d-none d-lg-block"><a class="nav-link bookmark-star"><i--}}
+{{--                    class="ficon feather icon-star warning"></i></a>--}}
+{{--                <div class="bookmark-input search-input">--}}
+{{--                  <div class="bookmark-input-icon"><i class="feather icon-search primary"></i></div>--}}
+{{--                  <input class="form-control input" type="text" placeholder="Explore Vuexy..." tabindex="0"--}}
+{{--                    data-search="laravel-search-list" />--}}
+{{--                  <ul class="search-list search-list-bookmark"></ul>--}}
+{{--                </div>--}}
+{{--                <!-- select.bookmark-select-->--}}
+{{--                <!--   option 1-Column-->--}}
+{{--                <!--   option 2-Column-->--}}
+{{--                <!--   option Static Layout-->--}}
+{{--              </li>--}}
+{{--            </ul>--}}
           </div>
           <ul class="nav navbar-nav float-right">
             <li class="dropdown dropdown-language nav-item">
