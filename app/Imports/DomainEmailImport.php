835dcf8d8d739806a34b6c67388c960fc44b014f
Sarmad Sohail <sarmad-sohail@hotmail.com>
2021-04-25 16:09:37 +0500

    updates

--- app/Imports/DomainEmailImport.php
+++ app/Imports/DomainEmailImport.php
@@ -2,16 +2,38 @@
 
 namespace App\Imports;
 
+use App\Domain;
+use App\DomainEmail;
 use Illuminate\Support\Collection;
 use Maatwebsite\Excel\Concerns\ToCollection;
+use Maatwebsite\Excel\Concerns\WithChunkReading;
+use Maatwebsite\Excel\Concerns\WithStartRow;
 
-class DomainEmailImport implements ToCollection
+class DomainEmailImport implements ToCollection, WithStartRow, WithChunkReading
 {
-    /**
-    * @param Collection $collection
-    */
-    public function collection(Collection $collection)
-    {
-        //
+  /**
+   * @param Collection $rows
+   */
+  public function collection(Collection $rows)
+  {
+    foreach ($rows as $row) {
+      $domain = Domain::whereName($row[1])->first();
+      if (!$domain) {
+        DomainEmail::create(["name" => $row[1], "email" => $row[17]]);
+      }
     }
+  }
+
+  /**
+   * @return int
+   */
+  public function startRow(): int
+  {
+    return 2;
+  }
+
+  public function chunkSize(): int
+  {
+    return 50;
+  }
 }
--- app/Imports/DomainPhoneImport.php
+++ app/Imports/DomainPhoneImport.php
@@ -2,16 +2,35 @@
 
 namespace App\Imports;
 
+use App\Domain;
+use App\DomainPhone;
 use Illuminate\Support\Collection;
 use Maatwebsite\Excel\Concerns\ToCollection;
+use Maatwebsite\Excel\Concerns\WithChunkReading;
+use Maatwebsite\Excel\Concerns\WithStartRow;
 
-class DomainPhoneImport implements ToCollection
+class DomainPhoneImport implements ToCollection, WithStartRow, WithChunkReading
 {
-    /**
-    * @param Collection $collection
-    */
-    public function collection(Collection $collection)
-    {
-        //
+  /**
+   * @param Collection $rows
+   */
+  public function collection(Collection $rows)
+  {
+    foreach ($rows as $row) {
+      $domain = Domain::whereName($row[1])->first();
+      if (!$domain) {
+        DomainPhone::create(["name" => $row[1], "phone" => $row[17]]);
+      }
     }
+  }
+
+  public function chunkSize(): int
+  {
+    return 50;
+  }
+
+  public function startRow(): int
+  {
+    return 2;
+  }
 }
