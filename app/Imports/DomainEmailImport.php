f670eb6b47273dfd60a3261de263f18161f025b6
Sarmad Sohail <sarmad-sohail@hotmail.com>
2021-05-02 16:01:56 +0500

    email api import

--- app/Imports/DomainEmailImport.php
+++ app/Imports/DomainEmailImport.php
@@ -4,6 +4,7 @@ namespace App\Imports;
 
 use App\Domain;
 use App\DomainEmail;
+use App\Helpers\Helper;
 use Illuminate\Support\Collection;
 use Maatwebsite\Excel\Concerns\ToCollection;
 use Maatwebsite\Excel\Concerns\WithChunkReading;
@@ -16,12 +17,47 @@ class DomainEmailImport implements ToCollection, WithStartRow, WithChunkReading
    */
   public function collection(Collection $rows)
   {
+    $util = new \App\Utils\Util();
     foreach ($rows as $row) {
       $domain = Domain::whereName($row[1])->first();
-      $domainEmail =DomainEmail::where(["name" => $row[1]])->first();
-      if(!$domainEmail)
-      {
-        DomainEmail::create(["name" => $row[1], "email" => $row[17]]);
+      $domainEmail = DomainEmail::where(["name" => $row[1]])->first();
+      if (!$domainEmail) {
+        $isPresent = true;
+        $websiteScrapedData = [];
+        try {
+          $body = $util->sitePresenceCheck($row[1]);
+        } catch (\Exception $exception) {
+          $isPresent = false;
+        }
+        if ($isPresent) {
+          $websiteScrapedData = (self::scrapeWebsiteData($body, $isPresent));
+          if ($isPresent) {
+            //Call to api to fetch social links,emails,phones,postal address
+            $data = \App\Helpers\Helper::getDomainInfo($row[1]);
+          }
+
+        }
+        if (($websiteScrapedData["title"] ?? false) && !Helper::detectChineseUTF8($websiteScrapedData["title"])) {
+          DomainEmail::create(
+            [
+              'name' => $row[1],
+              'email' => $row[17],
+              'register_date' => $row[2],
+              'server' => $row[7],
+              'owner_name' => $row[10],
+              'other_name' => $row[11],
+              'address' => $row[12],
+              'city' => $row[13],
+              'state' => $row[14],
+              'zip' => $row[15],
+              'country' => $row[16],
+              'title' => $websiteScrapedData["title"] ?? null,
+              'description' => $websiteScrapedData["description"] ?? null,
+            ]
+          );
+        }
+
+
       }
       if ($domain) {
         $domain->update(["email" => $row[17]]);
---
+++ database/migrations/2021_05_02_102759_alter_domain_phones_table.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AlterDomainPhonesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+  public function up()
+  {
+    Schema::table('domain_phones', function (Blueprint $table) {
+      $table->string('email')->nullable();
+      $table->string('register_date')->nullable();
+      $table->string('server')->nullable();
+      $table->string('owner_name')->nullable();
+      $table->string('other_name')->nullable();
+      $table->string('address')->nullable();
+      $table->string('city')->nullable();
+      $table->string('state')->nullable();
+      $table->string('zip')->nullable();
+      $table->string('country')->nullable();
+      $table->string('title')->nullable();
+      $table->string('description')->nullable();
+
+
+
+    });
+
+  }
+
+
+  /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
