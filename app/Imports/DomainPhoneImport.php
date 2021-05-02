362b303832f9801bac4cb3085746b67b1a195286
Sarmad Sohail <sarmad-sohail@hotmail.com>
2021-05-02 16:05:30 +0500

    domain phone import

--- app/Imports/DomainPhoneImport.php
+++ app/Imports/DomainPhoneImport.php
@@ -3,7 +3,9 @@
 namespace App\Imports;
 
 use App\Domain;
+use App\DomainEmail;
 use App\DomainPhone;
+use App\Helpers\Helper;
 use Illuminate\Support\Collection;
 use Maatwebsite\Excel\Concerns\ToCollection;
 use Maatwebsite\Excel\Concerns\WithChunkReading;
@@ -16,15 +18,52 @@ class DomainPhoneImport implements ToCollection, WithStartRow, WithChunkReading
    */
   public function collection(Collection $rows)
   {
+    $util = new \App\Utils\Util();
     foreach ($rows as $row) {
       $domain = Domain::whereName($row[1])->first();
-      $domainPhone =DomainPhone::where(["name" => $row[1]])->first();
-      if(!$domainPhone)
-      {
-        DomainPhone::create(["name" => $row[1], "phone" => $row[15]]);
+      $domainEmail = DomainPhone::where(["name" => $row[1]])->first();
+      if (!$domainEmail) {
+        $isPresent = true;
+        $websiteScrapedData = [];
+        try {
+          $body = $util->sitePresenceCheck($row[1]);
+        } catch (\Exception $exception) {
+          $isPresent = false;
+        }
+        if ($isPresent) {
+          $websiteScrapedData = (self::scrapeWebsiteData($body, $isPresent));
+          if ($isPresent) {
+            //Call to api to fetch social links,emails,phones,postal address
+            $data = \App\Helpers\Helper::getDomainInfo($row[1]);
+          }
+
+        }
+        if (($websiteScrapedData["title"] ?? false) && !Helper::detectChineseUTF8($websiteScrapedData["title"])) {
+          DomainPhone::create(
+            [
+              'name' => $row[1],
+              'phone_number' => $row[18],
+              'email'=>$row[17],
+              'register_date' => $row[2],
+              'server' => $row[7],
+              'owner_name' => $row[10],
+              'other_name' => $row[11],
+              'address' => $row[12],
+              'city' => $row[13],
+              'state' => $row[14],
+              'zip' => $row[15],
+              'country' => $row[16],
+              'title' => $websiteScrapedData["title"] ?? null,
+              'description' => $websiteScrapedData["description"] ?? null,
+            ]
+          );
+        }
+
+
       }
       if ($domain) {
-        $domain->update(["phone_number" => $row[17]]);
+        $domain->update(["phone_number" => $row[18]]);
+
       }
     }
   }
